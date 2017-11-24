c649e822d106275faab2a70598277a7c7620355f
Andrew Thomas <asthomas@Macintosh.local>
2017-11-24 16:34:17 -0500

    Added JavaScript for transcript highlighting

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 const vid = document.getElementsByTagName('video')[0];
+const span = document.getElementsByClassName('textWrapper').getElementsByTagName('span');
+
+//Script for highlighting transcript text as video plays
+
+vid.addEventListener('timeupdate', () => {
+  for (let i = 0; i < span.length; i += 1) {
+    let startTime = span[i].getAttribute('data-start');
+    let endTime = span[i].getAttribute('data-end');
+    let currentTime = vid.currentTime;
+    if (currentTime >= startTime && currentTime <= endTime) {
+      span[i].classList.add('highlight');
+    } else {
+      span[i].classList.remove('highlight');
+    }
+  }
+});
