f4aabf41873ccc7da279b621120104a84de52118
Andrew Thomas <asthomas@Macintosh.attlocal.net>
2017-11-25 00:11:01 -0500

    Updated script for highlighting transcript text

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 const vid = document.getElementsByTagName('video')[0];
-const span = document.getElementsByClassName('textWrapper').getElementsByTagName('span');
+const spans = document.querySelectorAll('.textWrapper span');
+
 
 //Script for highlighting transcript text as video plays
 
 vid.addEventListener('timeupdate', () => {
-  for (let i = 0; i < span.length; i += 1) {
-    let startTime = span[i].getAttribute('data-start');
-    let endTime = span[i].getAttribute('data-end');
+  for (let i = 0; i < spans.length; i += 1) {
+    let startTime = spans[i].getAttribute('data-start');
+    let endTime = spans[i].getAttribute('data-end');
     let currentTime = vid.currentTime;
     if (currentTime >= startTime && currentTime <= endTime) {
-      span[i].classList.add('highlight');
+      spans[i].classList.add('highlight');
     } else {
-      span[i].classList.remove('highlight');
+      spans[i].classList.remove('highlight');
     }
   }
 });
