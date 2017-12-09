e70caf2446fa8737a60c7fff3a2a3cb106106c3a
Andrew Thomas <asthomas@Macintosh.local>
2017-12-09 16:02:07 -0500

    Removed arrow functions for IE compatibility

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const spans = document.querySelectorAll('.textWrapper span');
 
 //Script for highlighting transcript text as video plays
 
-vid.addEventListener('timeupdate', () => {
+vid.addEventListener('timeupdate', function() {
   for (let i = 0; i < spans.length; i += 1) {
     let startTime = spans[i].getAttribute('data-start');
     let endTime = spans[i].getAttribute('data-end');
@@ -20,7 +20,7 @@ vid.addEventListener('timeupdate', () => {
 //Script for playing video after clicking a place in the transcript text
 
 function clickText(element) {
-   element.addEventListener("click", () => {
+   element.addEventListener("click", function() {
      let startTime = element.getAttribute('data-start');
      if (element) {
        vid.currentTime = startTime;
